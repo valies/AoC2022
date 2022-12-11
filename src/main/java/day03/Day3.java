256eba217871d6c26c434998096eb8327875907f
valies <da_valies@hotmail.com>
2022-12-11 23:33:39 +0100

    day 3 part 2

--- src/main/java/day03/Day3.java
+++ src/main/java/day03/Day3.java
@@ -5,24 +5,54 @@ import support.FileReader;
 import java.io.File;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Day3 {
     //Day 3: Rucksack reorganisation
+    //Find common item in two compartments
     public static int getResultOfPart1(File txtFile) throws Exception {
         String[] input = FileReader.transformToStringArray(txtFile);
         int result = 0;
-        for (String rucksack: input) {
-            List<Character> compartement1 = rucksack.substring(0, rucksack.length() / 2).chars().mapToObj(c -> (char) c).toList();
-            List<Character> compartement2 = rucksack.substring(rucksack.length() / 2).chars().mapToObj(c -> (char) c).toList();
-            Optional<Character> commonItemChar = compartement1.stream().filter(compartement2::contains).findFirst();
+        for (String rucksack : input) {
+            List<Character> compartment1 = rucksack.substring(0, rucksack.length() / 2).chars().mapToObj(c -> (char) c).toList();
+            List<Character> compartment2 = rucksack.substring(rucksack.length() / 2).chars().mapToObj(c -> (char) c).toList();
+            Optional<Character> commonItemChar = compartment1.stream().filter(compartment2::contains).findFirst();
             if (commonItemChar.isPresent()) {
-                int thisRucksacksResult = (int) commonItemChar.get() - 'a' + 1;
+                int thisResult = (int) commonItemChar.get() - 'a' + 1;
                 if (commonItemChar.get() >= 'A' && commonItemChar.get() <= 'Z') {
-                    thisRucksacksResult = (int) commonItemChar.get() - 'A' + 27;
+                    thisResult = (int) commonItemChar.get() - 'A' + 27;
                 }
-                result += thisRucksacksResult;
+                result += thisResult;
             } else {
-                throw new Exception("Can't find common item in " + compartement1 + " " + compartement2);
+                throw new Exception("Can't find common item in " + compartment1 + ", " + compartment2);
+            }
+        }
+        return result;
+    }
+
+    //Day 3: Rucksack reorganisation
+    //Find common item in 3 rugsacks
+    public static int getResultOfPart2(File txtFile) throws Exception {
+        String[] input = FileReader.transformToStringArray(txtFile);
+        int result = 0;
+        for (int i = 0; i < input.length; i += 3) {
+            List<Character> rugsack1 = input[i].chars().mapToObj(c -> (char) c).toList();
+            List<Character> rugsack2 = input[i + 1].chars().mapToObj(c -> (char) c).toList();
+            List<Character> rugsack3 = input[i + 2].chars().mapToObj(c -> (char) c).toList();
+            List<Character> commonItemChars1And2 = rugsack1.stream()
+                    .filter(rugsack2::contains).collect(Collectors.toList());
+            System.out.println(commonItemChars1And2);
+            Optional<Character> commonItemChar = commonItemChars1And2.stream()
+                    .filter(rugsack3::contains).findFirst();
+            System.out.println(commonItemChar);
+            if (commonItemChar.isPresent()) {
+                int thisResult = (int) commonItemChar.get() - 'a' + 1;
+                if (commonItemChar.get() >= 'A' && commonItemChar.get() <= 'Z') {
+                    thisResult = (int) commonItemChar.get() - 'A' + 27;
+                }
+                result += thisResult;
+            } else {
+                throw new Exception("Can't find common item in " + rugsack1 + ", " + rugsack2 + ", " + rugsack3);
             }
         }
         return result;
@@ -31,5 +61,6 @@ public class Day3 {
     public static void main(String[] args) throws Exception {
         File txtFile = new File("./src/main/java/day03/input.txt");
         System.out.println("Part 1: " + getResultOfPart1(txtFile));
+        System.out.println("Part 2: " + getResultOfPart2(txtFile));
     }
 }
--- src/test/java/day03/Day3Test.java
+++ src/test/java/day03/Day3Test.java
@@ -15,4 +15,12 @@ public class Day3Test {
         int actual = Day3.getResultOfPart1(txtFile);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void day3TestPart2() throws Exception {
+        File txtFile = new File("src/test/java/day03/input.txt");
+        int expected = 70;
+        int actual = Day3.getResultOfPart2(txtFile);
+        assertEquals(expected, actual);
+    }
 }
