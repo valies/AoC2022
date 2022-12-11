444988f2d64a92ecb85f654c450b35794dc2df72
valies <da_valies@hotmail.com>
2022-12-11 23:14:55 +0100

    day 3 part 2

--- src/test/java/day03/Day3Test.java
+++ src/test/java/day03/Day3Test.java
@@ -1,6 +1,5 @@
 package day03;
 
-import day02.Day2;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -14,14 +13,6 @@ public class Day3Test {
         File txtFile = new File("src/test/java/day03/input.txt");
         int expected = 157;
         int actual = Day3.getResultOfPart1(txtFile);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void day3TestPart2() throws Exception {
-        File txtFile = new File("src/test/java/day03/input.txt");
-        int expected = 70;
-        int actual = Day3.getResultOfPart2(txtFile);
         assertEquals(expected, actual);
     }
 }
